1c7048687855d302213a59158c301418238bd1ae
Tejas_Dibbad <tejas.dibbad@gmail.com>
2026-08-31 11:32:03 +0530

    Create RevStr.java

---
+++ RevStr.java
@@ -0,0 +1,23 @@
+public import java.util.Scanner;
+
+class RevStr
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for(int i = str.length() - 1; i >= 0; i--)
+        {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed string = " + reverse);
+    }
+} {
+    
+}
